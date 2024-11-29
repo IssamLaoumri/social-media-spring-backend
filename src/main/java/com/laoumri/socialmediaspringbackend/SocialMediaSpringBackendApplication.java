17c5feb1734067054071896edd84d56cfec48806
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2024-11-29 11:01:15 +0100

    chore: initial commit

---
+++ src/main/java/com/laoumri/socialmediaspringbackend/SocialMediaSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laoumri.socialmediaspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaSpringBackendApplication.class, args);
+    }
+
+}
